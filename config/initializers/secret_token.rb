929095adf6eb152ac314632ff734685de6c8536b
Yamile tobon <yamiletobon@misena.edu.co>
2013-04-25 09:48:06 -0500

    empezando con el proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Escuela::Application.config.secret_token = '16fa1370fd2a3d16d8dfd9dddfd743f433550e80534966b38638b1101ddd86d1b1bd959dfb15260b1e55bc9f4de5f494a2080616eddfb46461b836a1802c4fc8'
